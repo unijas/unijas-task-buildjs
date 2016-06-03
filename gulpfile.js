868961acd60b42611b35ef9bed815f8204a26a38
Siggi Duenkel <chaosprinz76@googlemail.com>
2016-06-03 19:37:17 +0200

    configuration-based build is working

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,71 @@
 'use strict'
 
 const gulp = require('gulp')
-const join = require('path').join
-const browserify = require('browserify')
-const source = require('vinyl-source-stream')
-const buffer = require('vinyl-buffer')
-const sourcemaps = require('gulp-sourcemaps')
-const babelify = require('babelify')
-const argv = require('yargs').argv
+const Join = require('path').join
+const Browserify = require('browserify')
+const Source = require('vinyl-source-stream')
+const Buffer = require('vinyl-buffer')
+const Sourcemaps = require('gulp-sourcemaps')
+const Babelify = require('babelify')
+const Uglify = require('gulp-uglify')
 
-let production = false
-if (argv.p) {
-  production.true
+/**
+ * ## readConfigFile
+ * require given file or the default-config-file and read in the stylus-conf
+ * @param  {[string]} file path to the config-file to use
+ * @return {[Object]}      stylus config-object
+ */
+let readConfigFile = function (file) {
+  let module
+  try {
+    module = require(file)
+  } catch (e) {
+    module = require(Join(__dirname, 'default.conf.json'))
+  }
+  console.log(module)
+  return module.client
 }
 
-let dependencies = [
-  'react',
-  'react-dom',
-  'jquery'
-]
-
-let destination = join('dist','js')
-if (production) {
-  destination = join('build', 'js')
+/**
+ * ## buildPathes
+ * @param  {[Array]} pathes Elements are file-glob-arrays
+ * @return {[Array]}        paths/file-globs as strings with prefixed cwd
+ */
+let buildPathes = function (pathes) {
+  pathes = pathes.map(path => {
+    return Join(process.cwd(), ...path)
+  })
+  return pathes
 }
+
+let clientConfig = readConfigFile(Join(process.cwd(), 'config', 'build.conf.json'))
+console.log(buildPathes(clientConfig.dest))
 gulp.task('buildjs:vendor', ()=> {
-  return browserify()
-    .require(dependencies)
+  return Browserify()
+    .require(clientConfig.dependencies)
     .bundle()
-    .pipe(source('vendor.js'))
-    .pipe(buffer())
-    .pipe(gulp.dest(destination))
+    .pipe(Source('vendor.js'))
+    .pipe(Buffer())
+    .pipe(Sourcemaps.init({loadMaps: true}))
+    .pipe(Uglify())
+    .pipe(Sourcemaps.write('.'))
+    .pipe(gulp.dest(buildPathes(clientConfig.dest)[0]))
 })
 
 gulp.task('buildjs:client', ()=> {
-  let common = browserify({
-    entries: join('src', 'bla.js'),
+  let common = Browserify({
+    entries: buildPathes(clientConfig.src),
     debug: true
-  }).external(dependencies)
-    .transform(babelify, {presets: ['es2015', 'react']})
+  }).external(clientConfig.dependencies)
+    .transform(Babelify, {presets: ['es2015', 'react']})
     .bundle()
-    .pipe(source('app.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(destination))
+    .pipe(Source(clientConfig.destFileName))
+    .pipe(Buffer())
+    .pipe(Sourcemaps.init({loadMaps: true}))
+    .pipe(Sourcemaps.write('.'))
+    .pipe(gulp.dest(buildPathes(clientConfig.dest)[0]))
   return common
 })
+
 
 module.exports = gulp.tasks
